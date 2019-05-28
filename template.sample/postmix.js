83729d24dbca698def294091408d95f4d4c46a51
Igor Khotin <khotin@gmx.com>
2019-05-28 21:15:23 +0300

    [+] empty and template samples

---
+++ template.sample/postmix.js
@@ -0,0 +1,12 @@
+// **********************************
+// sample bootstrap postmix js scirpt
+// **********************************
+
+// collider.jam runs this script during bootstrap
+// after mixing actual content from /mix
+// and executing bash postmix
+
+console.log('evaluating postmix js script')
+
+// TODO do your postmix work here
+
---
+++ template.sample/premix.js
@@ -0,0 +1,12 @@
+// *********************************
+// sample bootstrap premix js scirpt
+// *********************************
+
+// collider.jam runs this script during bootstrap
+// right after executing bash premix
+// and before mixing actual content from /mix
+
+console.log('evaluating premix js script')
+
+// TODO do your premix work here
+
